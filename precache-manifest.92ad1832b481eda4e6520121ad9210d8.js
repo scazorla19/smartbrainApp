c85340cb93cd7e6e8590b261218bc80e16eafb52
scazorla19 <rey@SkyNet.local>
2020-05-04 16:19:06 +1000

    Updates

---
+++ precache-manifest.92ad1832b481eda4e6520121ad9210d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fd80bb3b12b88ddbd239ac6f24c179",
+    "url": "/smartbrainApp/index.html"
+  },
+  {
+    "revision": "8664fd2a9326f7547c75",
+    "url": "/smartbrainApp/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "ec1965e8fef2da8ffbc8",
+    "url": "/smartbrainApp/static/css/main.e43630aa.chunk.css"
+  },
+  {
+    "revision": "8664fd2a9326f7547c75",
+    "url": "/smartbrainApp/static/js/2.a36b0805.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smartbrainApp/static/js/2.a36b0805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1965e8fef2da8ffbc8",
+    "url": "/smartbrainApp/static/js/main.b6a243d7.chunk.js"
+  },
+  {
+    "revision": "20ea334e0350595dc2a4",
+    "url": "/smartbrainApp/static/js/runtime-main.bfca90f1.js"
+  }
+]);
